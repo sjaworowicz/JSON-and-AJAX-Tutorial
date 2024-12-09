0e20f7877f59656bc069665b29b6107d805d8935
sjaworowicz <125sjaworowicz@frhsd.com>
2024-12-09 09:45:54 -0500

    21:53

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function(){
+    var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+  ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+  };
+  ourRequest.send();
+});
+
+function renderHTML(data) {
+
+}
+
 var thePets = [
     {
       "name": "Meowsalot",
@@ -12,12 +29,3 @@ var thePets = [
   ]
   
   thePets[1].favFood
-  
-  
-  var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-  ourRequest.onload = function() {
-    var ourData = JSON.parse(ourRequest.responseText);
-    console.log(ourData[0]);
-  };
-  ourRequest.send();
