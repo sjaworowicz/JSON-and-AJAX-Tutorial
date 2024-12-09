eaa06cb16defb461770e3dea5bffccad5c3a537c
sjaworowicz <125sjaworowicz@frhsd.com>
2024-12-09 09:29:06 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var thePets = [
+    {
+      "name": "Meowsalot",
+    "species": "cat",
+    "favFood": "tuna"
+    },
+    {
+      "name": "Barky",
+    "species": "dog",
+    "favFood": "carrots"
+    }
+  ]
+  
+  thePets[1].favFood
+  
+  
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+  ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    console.log(ourData[0]);
+  };
+  ourRequest.send();
