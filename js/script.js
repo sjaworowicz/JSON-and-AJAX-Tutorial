b5a98b7280132643383a06734717f7e3860bd542
sjaworowicz <125sjaworowicz@frhsd.com>
2024-12-10 10:55:52 -0500

    33:19

--- js/script.js
+++ js/script.js
@@ -1,31 +1,47 @@
-
+var pageCounter = 1;
+var animalContainer = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
 
-btn.addEventListener("click", function(){
+btn.addEventListener("click", function() {
     var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
   ourRequest.onload = function() {
     var ourData = JSON.parse(ourRequest.responseText);
     renderHTML(ourData);
   };
   ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add("hide-me");
+  }
 });
 
 function renderHTML(data) {
+  var htmlString = "";
 
+  for (i = 0; i < data.length; i++) {
+    htmlString += "<p>" + data[i].name + "is a" + data[i].species + "that likes to eat";
+
+    for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+      htmlString += data[i].foods.likes[ii];
+    }
+
+  }
+
+  animalContainer.insertAdjacentHTML('beforeend', htmlString);
 }
 
-var thePets = [
-    {
-      "name": "Meowsalot",
-    "species": "cat",
-    "favFood": "tuna"
-    },
-    {
-      "name": "Barky",
-    "species": "dog",
-    "favFood": "carrots"
-    }
-  ]
+// var thePets = [
+//     {
+//       "name": "Meowsalot",
+//     "species": "cat",
+//     "favFood": "tuna"
+//     },
+//     {
+//       "name": "Barky",
+//     "species": "dog",
+//     "favFood": "carrots"
+//     }
+//   ]
   
-  thePets[1].favFood
+//   thePets[1].favFood
