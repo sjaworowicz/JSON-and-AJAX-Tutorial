a27a26e1a3ae71544676b1f3fb71015f3af6367b
sjaworowicz <125sjaworowicz@frhsd.com>
2024-12-18 09:47:10 -0500

    got some ajax done, still somewhat confused with it

--- js/script.js
+++ js/script.js
@@ -1,31 +1,35 @@
 var pageCounter = 1;
-var animalContainer = document.getElementById("animal-info");
-var btn = document.getElementById("btn");
+$animalContainer = $("animal-info");
+var btn = $("#btn");
 
-btn.addEventListener("click", function() {
-    var ourRequest = new XMLHttpRequest();
-  ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
-  ourRequest.onload = function() {
-    if (ourRequest.status >= 200 && ourRequest.status < 400) {
-      var ourData = JSON.parse(ourRequest.responseText);
-      renderHTML(ourData);
-    } else {
-      console.log("We connected to the server, but it returned on error.");
+btn.on("click", function(){
+  $.ajax({
+    URL: 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json',
+    method: "GET",
+    success: function(result) {
+      animalContainer.append(result);
     }
-
-  };
-
-  ourRequest.oneerror = function() {
-    console.log("connection error");
-  };
-
-
-  ourRequest.send();
+  });
   pageCounter++;
-  if (pageCounter > 3) {
+ if (pageCounter > 3) {
     btn.classList.add("hide-me");
   }
 });
+
+  // ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+  // ourRequest.onload = function() {
+   // if (ourRequest.status >= 200 && ourRequest.status < 400) {
+     // var ourData = JSON.parse(ourRequest.responseText);
+    //  renderHTML(ourData);
+  //  } else {
+     // console.log("We connected to the server, but it returned on error.");
+  //  }
+
+ // };
+
+  // ourRequest.oneerror = function() {
+    // console.log("connection error");
+ // };
 
 function renderHTML(data) {
   var htmlString = "";
